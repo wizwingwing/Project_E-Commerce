e20c731f43d9fc059816f24c5db3507ef68041ba
wizwingwing <61519634+wizwingwing@users.noreply.github.com>
2021-04-08 19:14:30 +0700

    completed

--- src/Component/Card_flashsale.js
+++ src/Component/Card_flashsale.js
@@ -5,7 +5,15 @@ export default function Card_flashsale() {
     return (
         <div className="Card_flashsale Container">
             <div className="Card header">
-                JJJ
+            </div>
+                <p className="text-head">ชื่อสินค้า</p>
+                <span className="price">฿3,000</span>
+                <div className="Sale-body"></div>
+                <p  className="sale">฿6,000</p>
+                <span className="sale-percent">50%</span> 
+            
+            <div className="Button-Detail">
+                <button>รายละเอียด</button>
             </div>
         </div>
     )
